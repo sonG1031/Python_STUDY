fc519ee1ab300e0179186d3f6fef30d9d9717cc5
Minsub Song <minsub1031@gmail.com>
2022-11-25 17:08:38 +0900

    회사에서

---
+++ 14일차/code.py
@@ -0,0 +1,10 @@
+# 표준 입력으로 문자열이 입력됩니다. 입력된 문자열에서 'the'의 개수를 출력하는 프로그램을 만드세요(input에서 안내 문자열은 출력하지 않아야 합니다). 
+# 단, 모든 문자가 소문자인 'the'만 찾으면 되며 'them', 'there', 'their' 등은 포함하지 않아야 합니다.
+
+string = input().split()
+cnt = 0
+for i in string:
+    if 'the' == i.strip(',.'):
+        cnt += 1
+
+print(cnt)
