5b0a5c60a3d9ae402527beb302d1198248c712ad
송민섭 <minsub1031@gmail.com>
2023-01-03 17:25:18 +0900

    lambda expression

---
+++ 람다 표현식/practice_lambda.py
@@ -0,0 +1,10 @@
+# files = ['font', '1.png', '10.jpg', '11.gif', '2.jpg', '3.png', 'table.xslx', 'spec.docx']
+
+# print(list(filter(lambda x: x.find('.jpg') != -1 or x.find('.png') != -1, files)))
+
+
+files = input().split()
+
+result = list(map(lambda x: '{0:03d}.{1}'.format(int(x.split('.')[0]), x.split('.')[1]), files))
+
+print(result)
